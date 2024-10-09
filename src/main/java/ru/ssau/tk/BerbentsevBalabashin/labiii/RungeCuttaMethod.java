ca45f07943fd842ec9779aa683c20cfe9bc0c52d
Berbentsev <denberben@gmail.com>
2024-10-10 02:37:16 +0400

    y part

---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/RungeCuttaMethod.java
@@ -0,0 +1,25 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii;
+
+public class RungeCuttaMethod {
+    private final MathFunction function;
+
+    public RungeCuttaMethod(MathFunction function) {
+        this.function = function;
+    }
+
+    public double integrate(double x0, double y0, double x, int n) {
+        double h = (x - x0) / n;
+        double result = y0;
+
+        for (int i = 0; i < n; i++) {
+            double k1 = h * function.apply(x0);
+            double k2 = h * function.apply(x0 + h / 2);
+            double k3 = h * function.apply(x0 + h / 2);
+            double k4 = h * function.apply(x0 + h);
+
+            result += (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+            x0 += h;
+        }
+        return result;
+    }
+}
