8edc9728b3a67a56573eca4cad429ef562b67096
Balabashin <dsbalab0405@mail.ru>
2024-10-10 01:26:45 +0300

    x-part

---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/CompositeFunction.java
@@ -0,0 +1,15 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii;
+
+public class CompositeFunction implements MathFunction {
+    private final MathFunction firstFunc;
+    private final MathFunction secondFunc;
+
+    public CompositeFunction(MathFunction firstFunc, MathFunction secondFunc){
+        this.firstFunc = firstFunc;
+        this.secondFunc = secondFunc;
+    }
+    @Override
+    public double apply(double x) {
+        return secondFunc.apply(firstFunc.apply(x));
+    }
+}
