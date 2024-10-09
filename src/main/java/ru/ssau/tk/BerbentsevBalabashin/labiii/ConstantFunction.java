cd06ac281060ee709d29af39fec83b6a2bb3e903
Berbentsev <denberben@gmail.com>
2024-10-10 03:59:52 +0400

    y part

--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/ConstantFunction.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/ConstantFunction.java
@@ -4,18 +4,18 @@ public class ConstantFunction implements MathFunction {
 
     @Override
     public double apply(double x) {
+
         return constant;
     }
 
     public ConstantFunction(double constant){
+
         this.constant=constant;
     }
 
     private final double constant;
-
     public double GetConstant(){
         return constant;
     }
-
 }
 
