d81bd2ab0f88abb6e68754a635b21fcecd4aaf6a
Berbentsev <denberben@gmail.com>
2024-10-11 01:46:53 +0400

    xy test

--- src/test/java/ru/ssau/tk/BerbentsevBalabashin/labiii/ZeroFunctionTest.java
+++ src/test/java/ru/ssau/tk/BerbentsevBalabashin/labiii/ZeroFunctionTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ZeroFunctionTest {
+
     @Test
     public void validZero(){
         ZeroFunction ZeroFunction = new ZeroFunction();
