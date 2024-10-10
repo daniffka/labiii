14282ccd3b2b837ee4285440ad3ac346c9723257
Berbentsev <denberben@gmail.com>
2024-10-10 19:37:25 +0400

    y part

---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/AbstractTabulatedFunction.java
@@ -0,0 +1,30 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii;
+
+public abstract class AbstractTabulatedFunction implements TabulatedFunction {
+    protected int count;
+    protected abstract int floorIndexOfX(double x);
+    protected abstract double extrapolateLeft(double x);
+    protected abstract double extrapolateRight(double x);
+    protected abstract double interpolate(double x, int floorIndex);
+
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        return leftY + ( (x - leftX) * (rightY - leftY) / (rightX - leftX) );
+    }
+
+    public double apply(double x) {
+        if (x < leftBound()) {
+            return extrapolateLeft(x);
+        }
+        else if (x > rightBound()) {
+            return extrapolateRight(x);
+        }
+        else {
+            if (indexOfX(x) != -1) {
+                return getY(indexOfX(x));
+            }
+            else {
+                return interpolate(x, floorIndexOfX(x));
+            }
+        }
+    }
+}
