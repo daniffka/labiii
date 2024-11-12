c4be3aed107b4b978fc09c8092431e8465ce30df
Balabashin <dsbalab0405@mail.ru>
2024-11-12 16:09:52 +0300

    x part

--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/factory/ArrayTabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/factory/ArrayTabulatedFunctionFactory.java
@@ -1,4 +1,11 @@
 package ru.ssau.tk.BerbentsevBalabashin.labiii.functions.factory;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.TabulatedFunction;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.ArrayTabulatedFunction;
 
-public class ArrayTabulatedFunctionFactory {
+public class ArrayTabulatedFunctionFactory implements TabulatedFunctionFactory {
+
+    @Override
+    public TabulatedFunction create(double[] xValues, double[] yValues) {
+        return new ArrayTabulatedFunction(xValues, yValues);
+    }
 }
--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/factory/LinkedListTabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/factory/LinkedListTabulatedFunctionFactory.java
@@ -1,4 +1,10 @@
 package ru.ssau.tk.BerbentsevBalabashin.labiii.functions.factory;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.TabulatedFunction;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.LinkedListTabulatedFunction;
+public class LinkedListTabulatedFunctionFactory implements TabulatedFunctionFactory {
 
-public class LinkedListTabulatedFunctionFactory {
+    @Override
+    public TabulatedFunction create(double[] xValues, double[] yValues) {
+        return new LinkedListTabulatedFunction(xValues, yValues);
+    }
 }
