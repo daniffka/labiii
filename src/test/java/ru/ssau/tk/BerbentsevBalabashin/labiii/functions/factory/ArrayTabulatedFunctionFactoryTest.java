8b1670d1b7ae1e51dff9e30c209f917e32f73402
Balabashin <dsbalab0405@mail.ru>
2024-11-14 14:06:38 +0300

    x part

---
+++ src/test/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/factory/ArrayTabulatedFunctionFactoryTest.java
@@ -0,0 +1,16 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.functions.factory;
+import org.junit.jupiter.api.Test;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.TabulatedFunction;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.ArrayTabulatedFunction;
+import static org.junit.jupiter.api.Assertions.*;
+public class ArrayTabulatedFunctionFactoryTest {
+    @Test
+    public void testCreate() {
+        ArrayTabulatedFunctionFactory factory = new ArrayTabulatedFunctionFactory();
+        double[] xValues = {1, 4, 6, 8, 10};
+        double[] yValues = {1, 16, 36, 64, 100};
+        TabulatedFunction function = factory.create(xValues, yValues);
+
+        assertInstanceOf(ArrayTabulatedFunction.class, function);
+    }
+}
