1d5500ce1a1f4d953a059c9edde603251e45643a
Balabashin <dsbalab0405@mail.ru>
2024-11-14 23:29:36 +0300

    x part

--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/factory/LinkedListTabulatedFunctionFactory.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/factory/LinkedListTabulatedFunctionFactory.java
@@ -2,7 +2,6 @@ package ru.ssau.tk.BerbentsevBalabashin.labiii.functions.factory;
 import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.TabulatedFunction;
 import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.LinkedListTabulatedFunction;
 public class LinkedListTabulatedFunctionFactory implements TabulatedFunctionFactory {
-
     @Override
     public TabulatedFunction create(double[] xValues, double[] yValues) {
         return new LinkedListTabulatedFunction(xValues, yValues);
