fbd29de4d9afa5631d21832d32ba89d9dbfaf628
Balabashin <dsbalab0405@mail.ru>
2024-11-15 00:07:17 +0300

    y part

---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/io/FunctionsIO.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.io;
+
+public final class FunctionsIO {
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("Class is final");
+    }
+}
