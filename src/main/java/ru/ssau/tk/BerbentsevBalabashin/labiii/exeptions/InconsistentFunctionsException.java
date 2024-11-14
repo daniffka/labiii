2ab7fddc023a00a7823e5f0052b9704eec814d06
Berbentsev <denberben@gmail.com>
2024-11-14 16:46:57 +0400

    operations

---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/exeptions/InconsistentFunctionsException.java
@@ -0,0 +1,10 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.exeptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+
+    public InconsistentFunctionsException() {
+    }
+    public InconsistentFunctionsException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/MathFunction.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/MathFunction.java
@@ -3,4 +3,8 @@ package ru.ssau.tk.BerbentsevBalabashin.labiii.functions;
 public interface MathFunction {
     double apply(double x);
 
+    default CompositeFunction andThen(MathFunction afterFunction) {
+        return new CompositeFunction(this, afterFunction);
+    }
+
 }
