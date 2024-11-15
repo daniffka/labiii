4ba76489d0ffddea732e52950fa81d146d39a547
Berbentsev <denberben@gmail.com>
2024-11-16 00:33:47 +0400

    operations

--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/operations/TabulatedFunctionOperationService.java
@@ -2,8 +2,30 @@ package ru.ssau.tk.BerbentsevBalabashin.labiii.operations;
 
 import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.Point;
 import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.TabulatedFunction;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.factory.ArrayTabulatedFunctionFactory;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.factory.TabulatedFunctionFactory;
 
 public class TabulatedFunctionOperationService {
+
+    private TabulatedFunctionFactory factory;
+
+    public TabulatedFunctionOperationService(TabulatedFunctionFactory factory) {
+
+        this.factory = factory;
+    }
+
+    public TabulatedFunctionOperationService() {
+
+        this.factory = new ArrayTabulatedFunctionFactory();
+    }
+
+    public TabulatedFunctionFactory getFactory() {
+        return factory;
+    }
+
+    public void setFactory(TabulatedFunctionFactory factory) {
+        this.factory = factory;
+    }
 
     public static Point[] asPoints(TabulatedFunction tabulatedFunction) {
 
