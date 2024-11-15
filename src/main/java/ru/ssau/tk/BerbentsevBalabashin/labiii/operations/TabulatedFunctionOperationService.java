2c1bc33c13c4a2a897af1c42692641fb8e7c348b
Berbentsev <denberben@gmail.com>
2024-11-16 03:20:23 +0400

    method sum diff

--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/operations/TabulatedFunctionOperationService.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/operations/TabulatedFunctionOperationService.java
@@ -1,5 +1,6 @@
 package ru.ssau.tk.BerbentsevBalabashin.labiii.operations;
 
+import ru.ssau.tk.BerbentsevBalabashin.labiii.exeptions.InconsistentFunctionsException;
 import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.Point;
 import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.TabulatedFunction;
 import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.factory.ArrayTabulatedFunctionFactory;
@@ -37,4 +38,38 @@ public class TabulatedFunctionOperationService {
         }
         return points;
     }
+
+    private interface BiOperation{
+        double apply(double u, double v);
+
+    }
+
+    private TabulatedFunction doOperation(TabulatedFunction a, TabulatedFunction b, BiOperation operation){
+        if (a.getCount()!=b.getCount()) throw new InconsistentFunctionsException("arrays must have identical length");
+
+        Point[] pointA =asPoints(a);
+        Point[] pointB =asPoints(b);
+        double[] xValues=new double[pointA.length];
+        double[] yValues=new double[pointB.length];
+
+        for(int i=0; i<pointA.length; i++) {
+            if (pointA[i].getX() != pointB[i].getY())
+                throw new InconsistentFunctionsException("values must be the identical");
+
+            xValues[i] = pointA[i].getX();
+            yValues[i] = operation.apply(pointA[i].getY(),pointB[i].getY());
+        }
+    return factory.create(xValues,yValues);
+    }
+
+    public TabulatedFunction sum(TabulatedFunction a, TabulatedFunction b) throws InconsistentFunctionsException{
+        return doOperation(a,b,((u,v)-> u+v));
+
+    }
+
+    public TabulatedFunction diff(TabulatedFunction a, TabulatedFunction b) throws InconsistentFunctionsException{
+        return doOperation(a,b,((u,v)-> u-v));
+
+    }
+
 }
