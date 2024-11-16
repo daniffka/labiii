f546773ef4dac559b34e016df985f12ec6a2d47e
Berbentsev <denberben@gmail.com>
2024-11-16 05:42:26 +0400

    functionIo

--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/io/FunctionsIO.java
@@ -1,7 +1,27 @@
 package ru.ssau.tk.BerbentsevBalabashin.labiii.io;
 
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.Point;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.TabulatedFunction;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException("Class is final");
     }
+
+    void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function){
+        try(PrintWriter printWriter = new PrintWriter(writer)){
+            printWriter.println(function.getCount());
+            for (Point point: function){
+                printWriter.printf("%f %f\n",point.getX(),point.getY());
+            }
+            writer.flush();
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
 }
