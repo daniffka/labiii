23c1e0d209a5ed45f38ddc6d093b7d1bf2f05460
Balabashin <dsbalab0405@mail.ru>
2024-11-17 00:55:02 +0300

    x part

---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/concurrent/WriteTask.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.concurrent;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.functions.TabulatedFunction;
+
+public class WriteTask implements Runnable {
+    private final TabulatedFunction func;
+    private final double value;
+    public WriteTask(TabulatedFunction function, double value) {
+        this.func = function;
+        this.value = value;
+    }
+    public void run() {
+        for (int i = 0; i < func.getCount(); i++) {
+            func.setY(i, value);
+            System.out.printf("Writing for index %d complete\n", i);
+        }
+    }
+}
