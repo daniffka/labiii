8c12dde7a180dc03956d59b87c97958187063c5a
Berbentsev <denberben@gmail.com>
2024-11-19 02:52:37 +0400

    y part

--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/io/FunctionsIO.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/io/FunctionsIO.java
@@ -72,6 +72,13 @@ public final class FunctionsIO {
         }
 
     }
+
+    public static void serialize(BufferedOutputStream stream, TabulatedFunction function) throws IOException {
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(stream);
+        objectOutputStream.writeObject(function);
+        objectOutputStream.flush();
+    }
+
     public static TabulatedFunction deserialize(BufferedInputStream stream) throws IOException, ClassNotFoundException {
         ObjectInputStream objectInputStream = new ObjectInputStream(stream);
         return (TabulatedFunction) objectInputStream.readObject();
