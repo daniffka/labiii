4954427986c1375c4286916d58800391c85f4834
Balabashin <dsbalab0405@mail.ru>
2024-11-20 20:35:35 +0300

    x part

--- src/test/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/factory/LinkedListTabulatedFunctionFactoryTest.java
+++ src/test/java/ru/ssau/tk/BerbentsevBalabashin/labiii/functions/factory/LinkedListTabulatedFunctionFactoryTest.java
@@ -8,7 +8,7 @@ public class LinkedListTabulatedFunctionFactoryTest {
     public void testCreate() {
         LinkedListTabulatedFunctionFactory factory = new LinkedListTabulatedFunctionFactory();
         double[] xValues = {1, 4, 6 , 8, 10};
-        double[] yValues = {1, 16, 36, 4, 100};
+        double[] yValues = {1, 16, 36, 64, 100};
         TabulatedFunction function = factory.create(xValues, yValues);
         assertInstanceOf(LinkedListTabulatedFunction.class, function);
     }
