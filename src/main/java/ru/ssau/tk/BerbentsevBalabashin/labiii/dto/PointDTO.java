0e955a0b2f057b6e8de1439d91302444db7430cc
Balabashin <dsbalab0405@mail.ru>
2024-11-25 11:35:58 +0300

    x part

---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/dto/PointDTO.java
@@ -0,0 +1,20 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PointDTO {
+
+    private int id;
+
+    private int functionId;
+
+    private double xValue;
+
+    private double yValue;
+
+}
