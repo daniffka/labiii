7c15fe3a6b335ea89911146a8a22265319075358
Balabashin <dsbalab0405@mail.ru>
2024-11-25 18:48:13 +0300

    xy part

--- src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/entity/PointEntity.java
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/entity/PointEntity.java
@@ -1,32 +1,5 @@
 package ru.ssau.tk.BerbentsevBalabashin.labiii.entity;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
 
-@Entity
-@Table(schema = "labs", name = "t_point")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString(exclude = "functionEntity")
 public class PointEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private int id;
-
-    @Column(name = "c_x_value")
-    private double xValue;
-
-    @Column(name = "c_y_value")
-    private double yValue;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "function_id", nullable = false)
-    @JsonBackReference
-    private MathFunctionEntity functionEntity;
-
 }
