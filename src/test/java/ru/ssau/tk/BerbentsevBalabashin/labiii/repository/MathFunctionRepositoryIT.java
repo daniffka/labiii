6b7c9aa3c5eea579dbde2db909d913f2e392be14
Balabashin <dsbalab0405@mail.ru>
2024-11-27 12:47:29 +0300

    xy part

---
+++ src/test/java/ru/ssau/tk/BerbentsevBalabashin/labiii/repository/MathFunctionRepositoryIT.java
@@ -0,0 +1,38 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.repository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.transaction.annotation.Transactional;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.entity.MathFunctionEntity;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@Sql(scripts = {"/sql/test_schema.sql", "/sql/math_functions.sql"})
+@Transactional
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class MathFunctionRepositoryIT {
+
+    @Autowired
+    private MathFunctionRepository mathFunctionRepository;
+
+    @BeforeEach
+    public void setup() {
+        MathFunctionEntity function = new MathFunctionEntity();
+        function.setNameOfFunction("Linear");
+        this.mathFunctionRepository.save(function);
+    }
+
+    @Test
+    public void testFindByFunctionType_ReturnsFunctionList() {
+        // when
+        List<MathFunctionEntity> functions = this.mathFunctionRepository.findByFunctionType("Linear");
+
+        // then
+        assertEquals(1, functions.size());
+        assertEquals("Linear", functions.get(0).getNameOfFunction());
+    }
+}
---
+++ src/test/java/ru/ssau/tk/BerbentsevBalabashin/labiii/repository/MathFunctionRepositoryTest.java
@@ -0,0 +1,38 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.repository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import ru.ssau.tk.BerbentsevBalabashin.labiii.entity.MathFunctionEntity;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+class MathFunctionRepositoryTest {
+
+    @Mock
+    private MathFunctionRepository mathFunctionRepository;
+
+    private MathFunctionEntity functionEntity;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+        functionEntity = new MathFunctionEntity(1, "Linear", 2, 0.0, 10.0, null);
+    }
+
+    @Test
+    public void testFindByFunctionType() {
+        when(mathFunctionRepository.findByFunctionType("Linear")).thenReturn(Collections.singletonList(functionEntity));
+
+        List<MathFunctionEntity> functions = mathFunctionRepository.findByFunctionType("Linear");
+
+        assertEquals(1, functions.size());
+        assertEquals("Linear", functions.get(0).getNameOfFunction());
+        verify(mathFunctionRepository, times(1)).findByFunctionType("Linear");
+    }
+
+}
