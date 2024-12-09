94130cc3146a2f9e9eaac56a39400cd60b122ddf
Balabashin <dsbalab0405@mail.ru>
2024-12-09 20:41:07 +0300

    xy part

---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/entity/LabUser.java
@@ -0,0 +1,38 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.entity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CollectionIdJdbcTypeCode;
+
+@Entity
+@Table(schema = "db_schema", name = "t_lab_user")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LabUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "c_username")
+    @NotNull
+    @NotBlank
+    private String username;
+
+    @Column(name = "c_password")
+    @NotNull
+    @NotBlank
+    @Size(min = 8, max = 255)
+    private String password;
+
+    @Column(name = "c_role")
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private UserRole role;
+
+}
---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/entity/UserRole.java
@@ -0,0 +1,6 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.entity;
+
+public enum UserRole {
+    ADMIN,
+    CLIENT
+}
