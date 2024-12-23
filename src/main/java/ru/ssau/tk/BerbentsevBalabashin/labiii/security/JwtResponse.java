8d796253aa2877fe8ad4204a06d65d0654feedb6
Balabashin <dsbalab0405@mail.ru>
2024-12-23 18:46:39 +0300

    xy part

---
+++ src/main/java/ru/ssau/tk/BerbentsevBalabashin/labiii/security/JwtResponse.java
@@ -0,0 +1,14 @@
+package ru.ssau.tk.BerbentsevBalabashin.labiii.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class JwtResponse {
+
+    private String token;
+
+}
